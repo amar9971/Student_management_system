b509825970ec8bad31498356e58e27dc556cefee
amar9971 <rai.amarjeet31@gmail.com>
2023-12-05 01:00:59 +0530

    add some function

--- student/settings.py
+++ student/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    #'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +118,8 @@ STATIC_ROOT = 'api/static/'
 STATIC_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

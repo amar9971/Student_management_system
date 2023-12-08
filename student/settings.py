b6db05931bfc526c7517399720bc3c0e0abea43e
amar9971 <rai.amarjeet31@gmail.com>
2023-12-09 00:08:17 +0530

    add some function

--- student/settings.py
+++ student/settings.py
@@ -119,8 +119,8 @@ STATIC_ROOT = 'api/static/'
 STATIC_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
